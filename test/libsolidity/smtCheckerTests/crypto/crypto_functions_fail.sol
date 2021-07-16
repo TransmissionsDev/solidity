c67e58e9a4ad7e8ae4945c6180e7d578c8e96126
Leo Alt <leo@ethereum.org>
2021-07-16 15:44:36 +0200

    Update tests due to nondeterminism

--- test/libsolidity/smtCheckerTests/crypto/crypto_functions_fail.sol
+++ test/libsolidity/smtCheckerTests/crypto/crypto_functions_fail.sol
@@ -12,7 +12,8 @@ contract C {
 	function r(bytes memory b0, bytes memory b1) public pure {
 		bytes32 r0 = ripemd160(b0);
 		bytes32 r1 = ripemd160(b1);
-		assert(r0 == r1);
+		// Disabled because of Spacer nondeterminism.
+		//assert(r0 == r1);
 	}
 	function e(bytes32 h0, uint8 v0, bytes32 r0, bytes32 s0, bytes32 h1, uint8 v1, bytes32 r1, bytes32 s1) public pure {
 		address a0 = ecrecover(h0, v0, r0, s0);
@@ -25,8 +26,9 @@ contract C {
 // SMTEngine: all
 // SMTIgnoreCex: yes
 // ----
-// Warning 2072: (556-566): Unused local variable.
-// Warning 2072: (598-608): Unused local variable.
+// Warning 2072: (355-365): Unused local variable.
+// Warning 2072: (385-395): Unused local variable.
+// Warning 2072: (606-616): Unused local variable.
+// Warning 2072: (648-658): Unused local variable.
 // Warning 6328: (135-151): CHC: Assertion violation happens here.
 // Warning 6328: (272-288): CHC: Assertion violation happens here.
-// Warning 6328: (415-431): CHC: Assertion violation happens here.
